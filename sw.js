c90e9c074d1cda72975a2aa5a3a5ffb304dfcdeb
dong-yeol.lee <dong-yeol.lee@linecorp.com>
2026-05-12 15:02:53 +0900

    Initial commit - Let's Play Tennis web app

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'letsplaytennis-v2';
+const FILES = ['./index.html', './app.html', './privacy.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
